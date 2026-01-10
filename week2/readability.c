64c470e194ddab8de38a72692a06ebd45c36d40f
Nosignal777 <13396050406@163.com>
2026-01-09 21:15:13 -0600

    Add files via upload

---
+++ week2/readability.c
@@ -0,0 +1,74 @@
+#include <cs50.h>
+#include <ctype.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+
+int count_letters(string text);
+int count_words(string text);
+int count_sentences(string text);
+
+int main(void)
+{
+    string text = get_string("Text: ");
+
+    int letters = count_letters(text);
+    int words = count_words(text);
+    int sentences = count_sentences(text);
+
+    double L = letters * 100.0 / words;
+    double S = sentences * 100.0 / words;
+    int index = round(0.0588 * L - 0.296 * S - 15.8);
+
+    if (index < 1)
+    {
+        printf("Before Grade 1\n");
+    }
+    else if (index >= 16)
+    {
+        printf("Grade 16+\n");
+    }
+    else
+    {
+        printf("Grade %d\n", index);
+    }
+}
+
+int count_letters(string text)
+{
+    int n = 0;
+    for (int i = 0, len = strlen(text); i < len; i++)
+    {
+        if (isalpha(text[i]))
+        {
+            n++;
+        }
+    }
+    return n;
+}
+
+int count_words(string text)
+{
+    int n = 1;
+    for (int i = 0, len = strlen(text); i < len; i++)
+    {
+        if (isblank(text[i]))
+        {
+            n++;
+        }
+    }
+    return n;
+}
+
+int count_sentences(string text)
+{
+    int n = 0;
+    for (int i = 0, len = strlen(text); i < len; i++)
+    {
+        if (text[i] == '.' || text[i] == '?' || text[i] == '!')
+        {
+            n++;
+        }
+    }
+    return n;
+}
